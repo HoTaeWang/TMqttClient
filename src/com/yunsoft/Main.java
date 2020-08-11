7edb382c1a160bb15d55576a1c7ea16e88144a03
Tae-ho Kim <HoTaeWang@gmail.com>
2020-08-11 14:45:33 +0900

    MQTT example for eclipse IoT was added

--- src/com/yunsoft/Main.java
+++ src/com/yunsoft/Main.java
@@ -8,25 +8,42 @@ import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 public class Main {
 
-
     public static void main(String[] args) {
 
         System.out.println("Hello Mqtt");
         String topic = "MQTT APNC";
         String content = "Message from MqttPublish";
         int qos = 2;
-        String broker = "tcp://iot.eclipse.org:1883";
+
+        String broker = "tcp://mqtt.eclipse.org:1883";
+        String secureBroker = "tcp://mqtt.eclipse.org:8883";
         String clientId = "JavaSample";
         MemoryPersistence persistence = new MemoryPersistence();
 
         try{
             MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
+            // set Mqtt connect options
             MqttConnectOptions connectOptions = new MqttConnectOptions();
             connectOptions.setCleanSession(true);
+            // connect to broker
             System.out.println("Connecting to broker: " + broker);
             sampleClient.connect(connectOptions);
             System.out.println("Connected");
+            // Publish a message
+            System.out.println("Publish message: " + content );
+            MqttMessage message = new MqttMessage(content.getBytes());
+            message.setQos(qos);
+            sampleClient.publish(topic, message);
+            System.out.println("Message published");
+            sampleClient.disconnect();
+            System.out.println("Disconnected");
+            System.exit(0);
         }catch (MqttException e){
+            System.out.println("Reason: " + e.getReasonCode());
+            System.out.println("msg " + e.getMessage());
+            System.out.println("loc " + e.getLocalizedMessage());
+            System.out.println("cause " + e.getCause());
+            System.out.println("excep " + e);
             e.printStackTrace();
         }
 
